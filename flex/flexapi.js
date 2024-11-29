d1226b6d528926bd858f44148bf9dbfd135dea22
aaron <apuertollanoc@esliceu.net>
2024-11-29 20:06:20 +0100

    inicio tajer dor

--- flex/flexapi.js
+++ flex/flexapi.js
@@ -1,17 +1,15 @@
 
 const fetchCat = async () => {
-
     const res = await fetch("https://api.thecatapi.com/v1/images/search");
     const body = await res.json(); 
     return body[0]?.url; 
-
 };
 
 const loadGallery = async () => {
     const urls = await Promise.all([fetchCat(), fetchCat(), fetchCat(), fetchCat(), fetchCat(), fetchCat(), fetchCat(), fetchCat(), fetchCat()]);
     document.querySelector(".galery").innerHTML = urls
         .filter((url) => url) 
-        .map((url) => `<img src="${url}" alt="Cat Image" />`)
+        .map((url) => `<img src="${url}"/>`)
         .join("\n");
 };
 
