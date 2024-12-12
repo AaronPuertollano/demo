ec058c7446c3a0820f4dc64c52d867ae2c369083
aaron <apuertollanoc@esliceu.net>
2024-12-12 20:35:32 +0100

    contenido examen

--- main.js
+++ main.js
@@ -19,3 +19,22 @@ const promise2 = new Promise((resolve,reject) => {
         reject(new Error("Error!"));
     }
 });
+
+/*
+Implementa function partition(string, n), que donat un string i un número n, retorna un array amb els caràcters del string separats en arrays de n items.
+partition("Hello world!", 3) => [["H", "e", "l"], ["l", "o", " "], ["w", "o", "r"], ["l", "d", "!"]]
+partition("Hello world", 3) => [["H", "e", "l"], ["l", "o", " "], ["w", "o", "r"], ["l", "d"]]
+partition("Hello world", 2) => [["H", "e"], ["l", "l"], ["o", " "], ["w", "o"], ["r", "l"], ["d"]]
+
+Implementa function camelCase(string), que donat un string amb blancs, retorni el string equivalent en camel case.
+camelCase("Update user email") => "updateUserEmail"
+camelCase("Email") => "email"
+
+Implementa function fibonacciSeries(from, to), que retorna un array amb els números de la sequència de fibonacci entre from i to (inclusiu) (alerta: els arguments son les posicions del valors, no els propis valors). Assumim que la seqüència de fibonacci comença en 1: 1, 1, 2, 3, 5, 8, 13, 21, 34...
+fibonacciSeries(3, 7) => [3, 5, 8, 13, 21]
+fibonacciSeries(0, 1) => [1, 1]
+
+Què és una funció pura? Quins avantatges té emprar funcions pures? Posa exemples en codi de funcions pures i impures.
+
+Quines diferencies hi ha entre null i undefined? Què significa quan un valor és "falsy"? Posa exemples (amb codi) i comenta'ls.
+*/
